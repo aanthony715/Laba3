435bc460c901e5c265fcb0422c62a5b4196aa604
aanthony715 <156002637+aanthony715@users.noreply.github.com>
2026-01-16 17:47:15 +0500

    Update lab33.js

--- lab33.js
+++ lab33.js
@@ -1,17 +1,13 @@
 const fs = require('fs');
 const log = console.log;
 
-// Чтение и сортировка по двум параметрам
 const sortedStudents = require('./student.json')
     .sort((a, b) => {
-        // Сначала сравниваем по рейтингу (по убыванию)
         if (b.avg !== a.avg) {
             return b.avg - a.avg;
         }
-        // Если рейтинг одинаковый - по фамилии (по возрастанию)
         return a.lastName.localeCompare(b.lastName);
     });
 
-// Вывод результата
-log("=== ОТСОРТИРОВАННЫЙ СПИСКОК СТУДЕНТОВ ===");
+log("=== РћС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅС‹Р№ СЃРїРёСЃРѕРє СЃС‚СѓРґРµРЅС‚РѕРІ ===");
 log(JSON.stringify(sortedStudents, null, 4));
